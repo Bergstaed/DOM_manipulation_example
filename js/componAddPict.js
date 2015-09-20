2e780d6e5981a18ee7224a83a5282ef4f15e2177
Bergstaed <diemailbittezumir@gmx.de>
2015-09-20 12:22:12 +0200

    insert pict before js-do-xx

---
+++ js/componAddPict.js
@@ -0,0 +1,26 @@
+/**
+ * Created on 20.09.15.
+ */
+
+;
+( function( $, window, document, undefined ) {
+
+    $.fn.addPict = function() {
+
+        var mainKnot = $( this );
+        mainKnot.before($('<img class="addedPict" src="http://dummyimage.com/400x40/1b1db3/fff.jpg&text=Bergstaed">'));
+        //mainKnot.before($('<img class="addedPict" src="http://flickholdr.com/400/100">'));
+
+    }
+
+}( jQuery, this, this.document ));
+
+
+$( document ).ready(function() {
+
+    $( '.js-do-xx' ).each( function() {
+        $( this ).addPict();
+    });
+
+});
+
